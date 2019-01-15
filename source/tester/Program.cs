221a640953b9fca34a8b49f5d2840790d64cf29e
Henning Bredenkötter <hbre@users.noreply.github.com>
2019-01-15 14:20:59 +0100

    Font Tester Tool

---
+++ source/tester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Nuke.Common.OutputSinks;
+
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fontDir = @"D:\Privat\hbre-nuke-common\source\Nuke.Common\OutputSinks\Fonts\";
+
+            foreach (var font in new DirectoryInfo(fontDir).GetFiles("*.flf"))
+            {
+                string fontName = Path.GetFileNameWithoutExtension(font.FullName);
+                var text = FigletTransform.GetText("Hallo Welt 0123456789!", fontName);
+
+                Console.WriteLine(fontName);
+
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
